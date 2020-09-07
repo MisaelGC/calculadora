c8ba1c7586be4d269ef777c483f79cb10a57cc8c
MisaelGC <gacrmu@hotmail.com>
2020-09-07 16:24:23 -0500

    Funciones en click lista, cambios cuando se presiona los signos, decimales y el resultado

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ var p = {
     accion: null,
     digito: null,
     operaciones: document.querySelector("#operaciones"),
-    cantidadSignos: 0
+    cantidadSignos: 0,
+    cantidadDecimal: false,
+    resultado: false
 }
 
 // Objeto con los metódos de la calculadora
@@ -33,7 +35,12 @@ var m = {
                 if (p.operaciones.innerHTML == 0) {
                     p.operaciones.innerHTML = digito;
                 } else {
-                    p.operaciones.innerHTML += digito;
+                    if (p.resultado) {
+                        p.resultado = false;
+                        p.operaciones.innerHTML = digito;
+                    } else {
+                        p.operaciones.innerHTML += digito;
+                    }
                 }
                 break;
 
@@ -44,16 +51,22 @@ var m = {
                         p.operaciones.innerHTML = 0
                     } else {
                         p.operaciones.innerHTML += digito;
+                        p.cantidadDecimal = false;
+                        p.resultado = false;
                     }
                 }
                 break;
 
             case "decimal":
-                console.log("decimal");
+                if (!p.cantidadDecimal) {
+                    p.operaciones.innerHTML += digito;
+                    p.cantidadDecimal = true;
+                }
                 break;
 
             case "igual":
-                console.log("igual");
+                p.operaciones.innerHTML = eval(p.operaciones.innerHTML);
+                p.resultado = true;
                 break;
         }
     },
